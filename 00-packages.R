9c70271201e24b1f9d51ab6e3333f4aef7beb106
Ben Staton <statonbe@gmail.com>
2020-07-17 15:21:15 -0700

    Create 00-packages.R
    Place in the two packages used by the naming conventions document

---
+++ 00-packages.R
@@ -0,0 +1,2 @@
+library(knitr)          # for rendering Rmarkdown documents; install.packages("knitr")
+library(kableExtra)     # for building nice tables in Rmarkdown; install.packages("kableExtra")
