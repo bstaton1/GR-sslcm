4ccbd8f1f020c6805ce17d9d72966d2e96df52c3
Ben Staton <statonbe@gmail.com>
2020-09-25 13:06:45 -0700

    Add environmental "data prep" script
    This script currently just creates a PEU variable. In the future, this script will become more involved, and will read info from the GR-SSLCM-data repo

---
+++ 00-data/prep-env-data.R
@@ -0,0 +1,12 @@
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: #
+# SCRIPT TO PROCESS HABITAT/ENVIRONMENTAL DATA FILES FOR USE THROUGHOUT THIS PROJECT #
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: #
+
+##### INDEX OF CAPACITY: POOL EQUIVALENT UNITS #####
+
+PEU = c(
+  CAT = 12.44,
+  LOS = 12.21,
+  MIN = 37.45,
+  UGR = 14.79
+)
