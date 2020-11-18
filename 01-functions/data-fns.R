769100f1ab6b385502de0f8287650fd3e9baae3b
Ben Staton <statonbe@gmail.com>
2020-11-18 13:54:50 -0800

    bug fix: error in get_logit_se
    The previous method was for if you don't already have an SE. But if we don't have a lower CI, then we have the SE already

--- 01-functions/data-fns.R
+++ 01-functions/data-fns.R
@@ -6,10 +6,10 @@
 # some estimates have p_mean and CIs
 # some estimates have p_mean and p_se
 # either way, obtain CI's of proportion, then convert to a standard error on the logit scale
-get_logit_se = function(N, p_mean, p_se, p_lwr, p_upr) {
+get_logit_se = function(p_mean, p_se, p_lwr, p_upr) {
   # turn se's into ci if that is what is available
-  p_lwr = ifelse(is.na(p_se), p_lwr, p_mean + qnorm(0.025) * sqrt((p_mean * (1 - p_mean))/N) - (0.5/N))
-  p_upr = ifelse(is.na(p_se), p_upr, p_mean + qnorm(0.975) * sqrt((p_mean * (1 - p_mean))/N) + (0.5/N))
+  p_lwr = ifelse(is.na(p_se), p_lwr, p_mean + qnorm(0.025) * p_se)
+  p_upr = ifelse(is.na(p_se), p_upr, p_mean + qnorm(0.975) * p_se)
   
   # cap the CI
   p_lwr = ifelse(p_lwr <= 0, 0.001, p_lwr)
