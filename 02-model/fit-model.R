90cc806a5eac2827f838ac0c1ae8fd4cbcc65c6a
Ben <statonbe@gmail.com>
2023-04-05 09:50:00 -0700

    Rename mcmc_length options
    very_short -> vshort
    very_long -> vlong

--- 02-model/fit-model.R
+++ 02-model/fit-model.R
@@ -28,7 +28,7 @@ rmd = as.logical(args[4])
 if (is.na(scenario_name)) scenario_name = "siw-DF2"
 
 # set mcmc length
-if (is.na(mcmc_length)) mcmc_length = "very_short"
+if (is.na(mcmc_length)) mcmc_length = "vshort"
 
 # include a forward simulation to compare to observed values as a validation?
 if (is.na(do_sim_vs_obs)) do_sim_vs_obs = FALSE
@@ -270,11 +270,11 @@ if (do_lppd) jags_params = c(jags_params, lppd_params)
 ##### STEP 4: SELECT MCMC ATTRIBUTES #####
 
 jags_dims = list(
-  n_post = switch(mcmc_length,  "very_short" = 100,  "short" = 2000, "medium" = 24000, "long" = 50000, "very_long" = 100000),
-  n_burn = switch(mcmc_length,  "very_short" = 10,  "short" = 1000, "medium" = 20000, "long" = 30000, "very_long" = 50000),
-  n_thin = switch(mcmc_length,  "very_short" = 1,   "short" = 3,    "medium" = 8,     "long" = 10,    "very_long" = 25),
-  n_chain = switch(mcmc_length, "very_short" = 2,   "short" = 4,    "medium" = 4,     "long" = 4,     "very_long" = 4),
-  n_adapt = switch(mcmc_length, "very_short" = 100,  "short" = 10,  "medium" = 1000,  "long" = 3000,  "very_long" = 5000),
+  n_post  = switch(mcmc_length, "vshort" = 100, "short" = 2000, "medium" = 24000, "long" = 50000, "vlong" = 100000),
+  n_burn  = switch(mcmc_length, "vshort" = 10,  "short" = 1000, "medium" = 20000, "long" = 30000, "vlong" = 50000),
+  n_thin  = switch(mcmc_length, "vshort" = 1,   "short" = 3,    "medium" = 8,     "long" = 10,    "vlong" = 25),
+  n_chain = switch(mcmc_length, "vshort" = 2,   "short" = 4,    "medium" = 4,     "long" = 4,     "vlong" = 4),
+  n_adapt = switch(mcmc_length, "vshort" = 100, "short" = 100,  "medium" = 1000,  "long" = 3000,  "vlong" = 5000),
   parallel = TRUE
 )
 
